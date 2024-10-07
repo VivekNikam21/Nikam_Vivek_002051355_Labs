248edabadced28368c608bb11304987be8b7ffc3
VivekNikam21 <nikam.vi@northeastern.edu>
2024-10-06 21:27:58 -0400

    50%'

---
+++ OneDrive/Desktop/Aed Assign/Assignment_2/src/model/Address.java
@@ -0,0 +1,123 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author nikam
+ */
+public class Address {
+
+    private String hstreetadd;
+    private String hunitno;
+    private String hcity;
+    private String hstate;
+    private long hzip;
+    private double hphone;
+    private String wstreetadd;
+    private String wunitno;
+    private String wcity;
+    private String wstate;
+    private long wzip;
+    private double wphone;
+    
+    public String getHstreetadd() {
+        return hstreetadd;
+    }
+
+    public void setHstreetadd(String hstreetadd) {
+        this.hstreetadd = hstreetadd;
+    }
+
+    public String getHunitno() {
+        return hunitno;
+    }
+
+    public void setHunitno(String hunitno) {
+        this.hunitno = hunitno;
+    }
+
+    public String getHcity() {
+        return hcity;
+    }
+
+    public void setHcity(String hcity) {
+        this.hcity = hcity;
+    }
+
+    public String getHstate() {
+        return hstate;
+    }
+
+    public void setHstate(String hstate) {
+        this.hstate = hstate;
+    }
+
+    public long getHzip() {
+        return hzip;
+    }
+
+    public void setHzip(long hzip) {
+        this.hzip = hzip;
+    }
+
+    public double getHphone() {
+        return hphone;
+    }
+
+    public void setHphone(double hphone) {
+        this.hphone = hphone;
+    }
+
+    public String getWstreetadd() {
+        return wstreetadd;
+    }
+
+    public void setWstreetadd(String wtreetadd) {
+        this.wstreetadd = wtreetadd;
+    }
+
+    public String getWunitno() {
+        return wunitno;
+    }
+
+    public void setWunitno(String wunitno) {
+        this.wunitno = wunitno;
+    }
+
+    public String getWcity() {
+        return wcity;
+    }
+
+    public void setWcity(String wcity) {
+        this.wcity = wcity;
+    }
+
+    public String getWstate() {
+        return wstate;
+    }
+
+    public void setWstate(String wstate) {
+        this.wstate = wstate;
+    }
+
+    public long getWzip() {
+        return wzip;
+    }
+
+    public void setWzip(long wzip) {
+        this.wzip = wzip;
+    }
+
+    public double getWphone() {
+        return wphone;
+    }
+
+    public void setWphone(double wphone) {
+        this.wphone = wphone;
+    }
+    
+    
+}
